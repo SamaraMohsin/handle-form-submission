cef163bf478791d5ecfede40731eb5d1feb3bc83
SamaraMohsin2211014KHICND <samara.mohsin@xloopdigital.com>
2023-01-09 17:02:43 +0500

    first commit

---
+++ src/main/java/com/glc/handleformsubmission/HandleFormSubmissionApplication.java
@@ -0,0 +1,26 @@
+package com.glc.handleformsubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleFormSubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleFormSubmissionApplication.class, args);
+	}
+
+}
+
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class HandliFormSubmissionApplication {
+
+//   public static void main(String[] args) {
+//     SpringApplication.run(HandlingFormSubmissionApplication.class, args);
+//   }
+
+// }
---
+++ src/test/java/com/glc/handleformsubmission/HandleFormSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glc.handleformsubmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleFormSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
